e79c20650c9c30c53477a4c2e42ec6f6dc4cb1d6
Nazia.Habib <nazia.habib@patenergy.com>
2026-06-30 16:42:38 -0500

    load fit and input data

--- src/sensor_observations/models/sensors.py
+++ src/sensor_observations/models/sensors.py
@@ -1,6 +1,17 @@
 from sklearn.mixture import GaussianMixture
-from sklearn.cluster import KMeans
+import numpy as np
 
-def build_gmm(data):
-    model = GaussianMixture(n_components=5, random_state=0).fit(data) 
-    return model
+def get_prediction(fit_data, predict_data):
+    component_sizes = np.unique(fit_data[:, 0])
+    observations = fit_data[:, 1].reshape(-1, 1)
+
+    model = GaussianMixture(
+        n_components=len(component_sizes),
+        random_state=0
+    ).fit(observations)
+
+    component_order = np.argsort(model.means_.ravel())
+    probabilities = model.predict_proba(predict_data).round(3)
+    probabilities = probabilities[:, component_order]
+
+    return component_sizes, probabilities
